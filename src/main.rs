b8a15586337d90fc94fe4c8da6b69feaf580e7c5
Zettexe <zettexe@mailbox.org>
2023-08-15 21:10:49 +0200

    Add resizing and moving of window (janky as hell)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use hyprland::{
     data::Clients,
-    dispatch::{Dispatch, DispatchType, WindowIdentifier},
+    dispatch::{Dispatch, DispatchType, WindowIdentifier, Position},
     event_listener::EventListener,
     shared::HyprData,
 };
@@ -10,17 +10,26 @@ fn main() -> hyprland::Result<()> {
 
     println!("Started watching for windows");
 
+
     event_listener.add_window_title_change_handler(|address| {
         if let Some(client) = Clients::get()
             .unwrap()
             .find(|client| client.address.to_string() == format!("0x{address}"))
         {
             if client.title == "Picture-in-Picture" && !client.floating {
+                let window_dim_x: f32 = 1920.0;
+                let window_dim_y: f32 = 1080.0;
                 Dispatch::call(DispatchType::ToggleFloating(Some(WindowIdentifier::Title(
                     "Picture-in-Picture",
                 ))))
                 .unwrap();
-                println!("Floated window")
+                println!("Floated window");
+                Dispatch::call(DispatchType::ResizeActive(Position::Exact((window_dim_x * 0.3) as i16, (window_dim_y * 0.3) as i16)))
+                .unwrap();
+                println!("Resized window");
+                Dispatch::call(DispatchType::MoveActive(Position::Exact((window_dim_x - window_dim_x * 0.3) as i16 - 1, (window_dim_y - window_dim_y * 0.3) as i16 - 1)))
+                .unwrap();
+                println!("Moved window");
             }
         }
     });
